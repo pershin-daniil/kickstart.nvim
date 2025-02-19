f191dd8d9aad2e020d22be7c1e2f0fafe03ea892
Chris Patti <feoh@feoh.org>
2025-02-19 09:40:16 -0500

    Propsed fix for init.lua warnings as per https://github.com/nvim-lua/kickstart.nvim/issues/1305#issuecomment-2657770325 (#1354)

--- init.lua
+++ init.lua
@@ -710,8 +710,8 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
-        automatic_installation = {},
-        ensure_installed = {},
+        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+        automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
